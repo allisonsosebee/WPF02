6c4c1f3658e7ac5aed9fa5836e0f6e81d99b14c2
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-07 20:18:54 -0600

    width prompt

--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -7,5 +7,5 @@
 //How many bags of salt will you need to cover your icy sidewalk?
 
 var salt = prompt ("To find the amount of salt you'll need for your sidewalk, please enter the amount of square feet each bag will cover:")
-var width = prompt ("")
+var width = prompt ("Next, how wide is your sidewalk in feet?")
 var length = prompt ("")
