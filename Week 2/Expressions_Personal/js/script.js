36bf95c9e28b8a1b55259f7c06da5d272bc00f39
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-07 20:21:19 -0600

    comments

--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -8,5 +8,6 @@
 
 //The first variable used in the equation- the amount of square feet each bag will cover
 var salt = prompt ("To find the amount of salt you'll need for your sidewalk, please enter the amount of square feet each bag will cover:")
+//the second variable used in the equation- the width of the sidewalk that needs to be covered
 var width = prompt ("Next, how wide is your sidewalk in feet?")
 var length = prompt ("Lastly, please enter the length of your sidewalk in feet:")
