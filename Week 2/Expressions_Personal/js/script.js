ef9caee19ac905a3df7a24cf23f0a466b789db11
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-07 20:19:19 -0600

    length prompt

--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -8,4 +8,4 @@
 
 var salt = prompt ("To find the amount of salt you'll need for your sidewalk, please enter the amount of square feet each bag will cover:")
 var width = prompt ("Next, how wide is your sidewalk in feet?")
-var length = prompt ("")
+var length = prompt ("Lastly, please enter the length of your sidewalk in feet:")
