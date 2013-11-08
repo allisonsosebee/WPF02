dda85397a4642162bf8d4bb1d0ba2f0b05346d13
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-07 20:54:40 -0600

    corrections

--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -7,7 +7,7 @@
 //How many bags of salt will you need to cover your icy sidewalk?
 
 //the first variable used in the equation- the amount of square feet each bag will cover
-var salt = prompt ("To find the amount of salt you'll need for your sidewalk, please enter the amount of square feet each bag will cover:");
+var salt = prompt ("To find the amount of salt you'll need for your sidewalk, \nplease enter the amount of square feet each bag will cover:");
 //the second variable used in the equation- the width of the sidewalk that needs to be covered
 var width = prompt ("Next, how wide is your sidewalk in feet?");
 //the third variable used in the equation- the length of the sidewalk that needs to be covered
