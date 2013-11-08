b57ef1951087969aacee51ec9ca7990d09669127
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-07 20:17:24 -0600

    salt prompt

--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -6,6 +6,6 @@
 //Calculator Use:
 //How many bags of salt will you need to cover your icy sidewalk?
 
-var salt = prompt ("")
+var salt = prompt ("To find the amount of salt you'll need for your sidewalk, please enter the amount of square feet each bag will cover:")
 var width = prompt ("")
 var length = prompt ("")
