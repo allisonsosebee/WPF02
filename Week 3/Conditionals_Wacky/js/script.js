31cf59278f660dfacc958d9f66892f483e423183
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-14 16:49:36 -0600

    time

--- Week 3/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Wacky/js/script.js
@@ -7,3 +7,4 @@
 //Do you qualify for heath benefits or retirement?
 
 var age = prompt("To find if you're eligible for retirement or \nhealth benefits, Please enter your age:");
+var time = prompt()
