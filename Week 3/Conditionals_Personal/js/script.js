95627d67d01f18c7d5ffe481e2107019c4fb9995
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-14 16:35:42 -0600

    commute time

--- Week 3/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Personal/js/script.js
@@ -6,3 +6,5 @@
 //Calculator Use:
 //This calculator will determine a driver's commute time with construction traffic
 
+//the first variable used in the equation- the number of items to be purchased
+var commute = prompt("Enter your usual commute time in minutes:");
