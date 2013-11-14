a2b6f847fb52d69cd80ad6a5bae5c206a828d4c4
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-14 15:05:17 -0600

    number of items

--- Week 3/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Industry/js/script.js
@@ -6,4 +6,5 @@
 //Calculator Use:
 //Determine the total cost after discount depending on number of items sold.
 
+var numItems = prompt("To find the discounted price of your purchase, \nPlease enter the number of items you purchased:");
 
