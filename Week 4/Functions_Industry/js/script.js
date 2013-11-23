d6e6bdcc23bf730cc817ff91e1556191b2daff95
Allison Sosebee <Henry@Henrys-MacBook-Pro.local>
2013-11-22 19:05:27 -0600

    comments

--- Week 4/Functions_Industry/js/script.js
+++ Week 4/Functions_Industry/js/script.js
@@ -7,11 +7,10 @@
 //Use this calculator to see if you've met your sales goal for the day
 
 
-
 //anonymous function with parameters
 var salesGoal = function (goal, sales){
+    //calling out the variable "answer"
     var answer;
-
     answer = (goal > sales || goal <= sales) ? "Keep it up, you haven't met your goal yet!" : "Congratulations! You met your goal for the day!"
     alert (answer);
     return answer;
